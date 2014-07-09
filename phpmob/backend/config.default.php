239852e235bd1dd883fff0dfa2ebd86a8ed95407
Filipe Oliveira <contato@fmoliveira.com.br>
2014-07-09 06:19:00 -0300

    Configurações do site.

--- phpmob/backend/config.default.php
+++ phpmob/backend/config.default.php
@@ -1,16 +1,24 @@
 <?php
 require_once 'library/facebook.php';
+require_once 'util.php';
 
+/* Configurações do aplicativo no Facebook. */
 $app_id = "{app_id}";
 $app_secret = "{app_secret}";
 
-$admin_fbid = "{user_id}";
+/* Endereço do website. */
+$website_ssl = false;
+$website_proto = ($website_ssl == true ? "https" : "http");
+$website_host = $_SERVER["HTTP_HOST"];
+$website_root = "";
 
+/* Dados de conexão ao banco de dados. */
 $mysql_hostname = "{mysql_hostname}";
 $mysql_database = "{mysql_database}";
 $mysql_username = "{mysql_username}";
 $mysql_password = "{mysql_password}";
 
+/* Inicializa a API do Facebook. */
 $facebook = new Facebook(array(
 	'appId' => $app_id,
 	'secret' => $app_secret,
