712d95238fbe74b7d8c83bc1464ac0f5ced00924
Filipe Oliveira <contato@fmoliveira.com.br>
2014-07-13 09:14:40 -0300

    Dividindo para conquistar.

---
+++ phpmob/footer.inc.php
@@ -0,0 +1,30 @@
+<footer class="bs-docs-footer" role="contentinfo">
+	<div class="container">
+		<div class="bs-docs-social">
+			<div class="fb-like-box" data-href="https://www.facebook.com/pages/Mob-Your-Face/1446731825581145" data-width="400" data-height="210" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
+		</div>
+		
+		<ul class="bs-docs-footer-links muted">
+			<li><a href="http://www.fmobstudio.com.br">F&gt;MOB STUDIO</a></li>
+			<li>&middot;</li>
+			<li><a href="./missao">Missão</a></li>
+			<li>&middot;</li>
+			<li><a href="./visao">Visão</a></li>
+			<li>&middot;</li>
+			<li><a href="./valores">Valores</a></li>
+			<li>&middot;</li>
+			<li><a href="./suporte-tecnico">Suporte Técnico</a></li>
+		</ul>
+		
+		<p>Mob Your Face &copy; 2014 - Todos os direitos reservados</p>
+	</div>
+</footer>
+
+<div id="fb-root"></div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/facebook.js"></script>
--- phpmob/index.php
+++ phpmob/index.php
@@ -2,42 +2,12 @@
 require_once "login.inc.php";
 ?>
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
-
-    <title>Mob Your Face</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-	
-	<!-- Documentation extras -->
-	<link href="css/docs.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/mobyourface.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
+<html lang="pt-br">
+  <?php include("header.inc.php"); ?>
   <body>
-	<div id="fb-root"></div>
 
     <?php require "navbar.inc.php"; ?>
 
-    <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
         <h1>Hello, world!</h1>
@@ -47,7 +17,6 @@ require_once "login.inc.php";
     </div>
 
     <div class="container">
-      <!-- Example row of columns -->
       <div class="row">
         <div class="col-md-4">
           <h2>Heading</h2>
@@ -65,36 +34,8 @@ require_once "login.inc.php";
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div>
       </div>
-    </div> <!-- /container -->
+    </div>
 	
-	<footer class="bs-docs-footer" role="contentinfo">
-		<div class="container">
-			<div class="bs-docs-social">
-				<div class="fb-like-box" data-href="https://www.facebook.com/pages/Mob-Your-Face/1446731825581145" data-width="400" data-height="210" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
-			</div>
-
-		<p>Mob Your Face &copy; 2014 - Todos os direitos reservados</p>
-		
-		<ul class="bs-docs-footer-links muted">
-			<li><a href="http://www.fmobstudio.com.br">F&gt;MOB STUDIO</a></li>
-			<li>&middot;</li>
-			<li><a href="./missao">Missão</a></li>
-			<li>&middot;</li>
-			<li><a href="./visao">Visão</a></li>
-			<li>&middot;</li>
-			<li><a href="./valores">Valores</a></li>
-			<li>&middot;</li>
-			<li><a href="./suporte-tecnico">Suporte Técnico</a></li>
-		</ul>
-		</div>
-	</footer>
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/facebook.js"></script>
+	<?php require "footer.inc.php"; ?>
   </body>
 </html>
