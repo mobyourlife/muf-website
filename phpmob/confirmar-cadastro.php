8c8ecb8f1d54b7e1909fbf4839c77cda1c9f7ce6
Filipe Oliveira <contato@fmoliveira.com.br>
2014-07-14 22:16:08 -0300

    Iniciando página de confirmação de cadastro.

--- phpmob/confirmar-cadastro.php
+++ phpmob/confirmar-cadastro.php
@@ -1,11 +1,18 @@
 <?php
 require_once "core.inc.php";
-require_once "facebook.inc.php";
+require_once "muf-user.inc.php";
 
-if (isset($fb_profile))
+/* Redireciona o usuário para a página de login. */
+if (!isset($fb_profile))
+{
+	header("Location: " . $website_root . "/login-social");
+}
+/* Redireciona o usuário para o painel do usuário. */
+else if ($muf_registered === true)
 {
 	header("Location: " . $website_root . "/painel");
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -16,8 +23,8 @@ if (isset($fb_profile))
 
     <div class="jumbotron">
       <div class="container">
-        <h1>Login social</h1>
-        <p>Efetue login através de seu Facebook para começar.</p>
+        <h1>Confirmar cadastro</h1>
+        <p>Por favor verifique os seus dados antes de continuar.</p>
       </div>
     </div>
 
@@ -25,11 +32,11 @@ if (isset($fb_profile))
 		<div class="process">
 			<div class="process-row">
 				<div class="process-step">
-					<button type="button" class="btn btn-info btn-circle" disabled="disabled"><i class="fa fa-facebook fa-3x"></i></button>
+					<button type="button" class="btn btn-success btn-circle" disabled="disabled"><i class="fa fa-facebook fa-3x"></i></button>
 					<p>Login social</p>
 				</div>
 				<div class="process-step">
-					<button type="button" class="btn btn-default btn-circle" disabled="disabled"><i class="fa fa-pencil fa-3x"></i></button>
+					<button type="button" class="btn btn-info btn-circle" disabled="disabled"><i class="fa fa-pencil fa-3x"></i></button>
 					<p>Confirmar cadastro</p>
 				</div>
 				<div class="process-step">
@@ -41,14 +48,6 @@ if (isset($fb_profile))
 					<p>Pronto!</p>
 				</div> 
 			</div>
-		</div>
-		<div class="center">
-			<p>
-				Clique no botão abaixo para efetuar login.
-			</p>
-			<p>
-				<button id="btn-fblogin" class="btn btn-lg btn-primary" onclick="facebookPopup('<?php print($fb_loginurl); ?>', '<?php print($fb_panelurl); ?>'); "><span class="fa fa-facebook jump-5"></span> Entrar com Facebook</button>
-			</p>
 		</div>
 	</div>
 	
--- phpmob/painel.php
+++ phpmob/painel.php
@@ -1,23 +1,6 @@
 <?php
 require_once "core.inc.php";
-
-/* Verifica se o usuário está autenticado. */
-if (!isset($fb_profile))
-{
-	header("Location: " . $website_root . "/login-social");
-}
-
-/* Conecta-se ao banco de dados. */
-$db = mysql_connect($mysql_hostname, $mysql_username, $mysql_password);
-mysql_select_db($mysql_database, $db);
-
-/* Verifica se o usuário está cadastrado. */
-$sql = sprintf("SELECT 1 FROM mobface_facebookuser WHERE fb_uid = %d;", $fb_profile->getProperty('id'));
-$res = mysql_query($sql);
-$muf_registered = (mysql_num_rows($res) == 1) ? true : false;
-
-/* Fecha a conexão com o banco de dados. */
-mysql_close($db);
+require_once "muf-user.inc.php";
 
 /* Redireciona o usuário para a página de cadastro. */
 if ($muf_registered === false)
